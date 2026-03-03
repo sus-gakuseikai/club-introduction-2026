e0871a2da2783274a1bee3367bd307bb1101d80e
mi-1200 <kafupanpakazu2434@gmail.com>
2026-03-03 21:14:07 +0900

    Refactor club data in publish_data.js
    Removed several club entries and updated one club's details.

--- js/publish_data.js
+++ js/publish_data.js
@@ -190,50 +190,8 @@ const publish_info = [
   {
     category: "文化系",
     clubs: [
-      { clubName: 'JAZZ研究会',
-        id: 'jazz',
-        isMovie: false,
-        movieUrl: '',
-        links: [
-            {
-              label: 'Jazz研究会Instagram',
-              url: 'https://www.instagram.com/jazz_ken_tus/'
-            },
-            {
-              label: 'Jazz研究会SOLA',
-              url: 'https://sola.sus.ac.jp/tag/index.php?tc=1&tag=JAZZ研究会'
-            },
-        ]
-      },
-     { clubName: 'SUWAGEEKS',
-        id: 'suwageeks',
-        isMovie: false,
-        movieUrl: '',
-        links: [
-            {
-            },
-        ]
-      },
-       { clubName: 'AIサークル',
-        id: 'AI',
-        isMovie: false,
-        movieUrl: '',
-        links: [
-            {
-            },
-        ]
-      },
-    　{ clubName: 'ボードゲーム部',
-        id: 'board_game',
-        isMovie: false,
-        movieUrl: '',
-        links: [
-            {
-            },
-        ]
-      },
-       { clubName: 'ロボット研究部',
-        id: 'robot',
+       { clubName: '',
+        id: 'tennis',
         isMovie: false,
         movieUrl: '',
         links: [
@@ -249,87 +207,7 @@ const publish_info = [
           {
           },
         ]
-      },
-       { clubName: '写真部',
-        id: 'photograph',//intro_pngの中のファイル名。拡張端子はpngのみ
-        isMovie: false,
-        movieUrl: '',
-        links: [
-            {
-              label: '写真部X',
-              url: 'https://x.com/suwarika_photo'
-            },
-        ]
-      },
-       { clubName: '軽音学部',
-        id: 'K-ON',
-        isMovie: false,
-        movieUrl: '',
-        links: [
-            {
-              label: '軽音部 X',
-              url: 'https://x.com/sus_schoolband'
-            },
-            {
-              label: '軽音部 YouTube',
-              url: 'https://www.youtube.com/channel/UCpUM8-AZ0zxWZbyvgnzLMdg'
-            },
-            {
-              label: '軽音部 Instagram',
-              url: 'https://www.instagram.com/sus_schoolband/'
-            },
-        ]
-      },
-       { clubName: 'TRPG部',
-        id: 'TRPG',
-        isMovie: false,
-        movieUrl: '',
-        links: [
-            {
-              label: '連絡先（部長：渡辺）',
-              url: 't124171@ed.sus.ac.jp'
-            },
-        ]
-      },
-      { clubName: 'ってみた部',
-        id: 'ttemita',
-        isMovie: false,
-        movieUrl: '',
-        links: [
-            {
-              label: 'ってみた部 X',
-              url: 'http://x.com/ttemitabu_tus'
-            },
-            {
-              label: 'ってみた部 Youtube',
-              url: 'https://www.youtube.com/channel/UCwiwpf8uEk4lI3LpV3KL7QQ'
-            },
-            {
-              label: 'ってみた部 Instagram',
-              url: 'https://www.instagram.com/ttemitabu?igsh=MTBkOW5vc3ljM29wYw%3D%3D&utm_source=qr'
-            },
-        ]
-      },
-      { clubName: 'Poly Talk部',
-        id: 'poly_talk',
-        isMovie: false,
-        movieUrl: '',
-        links: [
-            {
-              label: 'Poly Talk部お問い合わせform',
-              url: 'https://docs.google.com/forms/d/e/1FAIpQLScSH6CpjA50RyCo3boCq267bebIl6O8HaVp7zwNKtlN8z31Xg/viewform?usp=dialog'
-            },
-        ]
-      },
-      { clubName: 'コミック研究部',
-        id: 'comic',
-        isMovie: false,
-        movieUrl: '',
-        links: [
-            {
-            },
-        ]
-      },
+      }
     ]
   },
   {
