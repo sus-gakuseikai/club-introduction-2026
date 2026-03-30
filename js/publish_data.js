55867b15a1d0b23bf01148f0d4545aac339014c8
mi-1200 <kafupanpakazu2434@gmail.com>
2026-03-30 21:54:19 +0900

    Update publish_data.js

--- js/publish_data.js
+++ js/publish_data.js
@@ -2,296 +2,212 @@ const publish_info = [
   {
     category: 'お知らせ',
     clubs: [
-      { clubName: '部活動説明会',
-        id: 'explanation',//intro_pngの中の名前。拡張端子はpngのみ
+      {
+        clubName: '部活動説明会',
+        id: 'explanation', // intro_pngの中の名前。拡張端子はpngのみ
         isMovie: false,
         movieUrl: '',
         links: [
-            {
-              label: '※参加団体は変更することがあります。',
-              url: ''
-            },
+          {
+            label: '※参加団体は変更することがあります。',
+            url: ''
+          }
         ]
-      },
+      }
     ]
   },
-  
   {
     category: '総務系',
     clubs: [
-      { clubName: '学生会',
-        id: 'gakuseikai',//intro_pngの中の名前。拡張端子はpngのみ
+      {
+        clubName: '学生会',
+        id: 'gakuseikai',
         isMovie: false,
         movieUrl: '',
         links: [
-            {
-              label: 'X(旧Twitter)',
-              url: 'https://x.com/suwa_gakuseikai'
-            },
-        ]
-      },
-      { clubName: '新風祭実行委員',
-        id: 'shinpusai',//intro_pngの中の名前。拡張端子はpngのみ
-        isMovie: false,
-        movieUrl: '',
-        links: [
-            {
-              label: '新風祭実行委員会 X',
-              url: 'https://x.com/shinpusai?s=21&t=YkpT3R0_ciyGPLkF71Nqig'
-            },
           {
-              label: '新風祭実行委員会 Instagram',
-              url: 'https://www.instagram.com/suwarika_festival?igsh=cTZ0bm1icjRzNnd4'
-            },{
-              label: '新風祭実行委員会 ホームページ',
-              url: 'https://recsuwa.net/'
-            },
+            label: 'X(旧Twitter)',
+            url: 'https://x.com/suwa_gakuseikai'
+          }
         ]
       },
+      {
+        clubName: '新風祭実行委員',
+        id: 'shinpusai',
+        isMovie: false,
+        movieUrl: '',
+        links: [
+          {
+            label: '新風祭実行委員会 X',
+            url: 'https://x.com/shinpusai?s=21&t=YkpT3R0_ciyGPLkF71Nqig'
+          },
+          {
+            label: '新風祭実行委員会 Instagram',
+            url: 'https://www.instagram.com/suwarika_festival?igsh=cTZ0bm1icjRzNnd4'
+          },
+          {
+            label: '新風祭実行委員会 ホームページ',
+            url: 'https://recsuwa.net/'
+          }
+        ]
+      }
     ]
   },
   {
     category: '運動系',
     clubs: [
-      { clubName: 'WORKOUT',
+      {
+        clubName: 'WORKOUT',
         id: 'workout',
         isMovie: false,
         movieUrl: '',
-        links: [
-            {
-            },
-        ]
+        links: []
       },
-      { clubName: '剣道部',
+      {
+        clubName: '剣道部',
         id: 'kendou',
         isMovie: false,
         movieUrl: '',
-        links: [
-            {
-            },
-        ]
+        links: []
       },
-      { clubName: '軟式野球部',
+      {
+        clubName: '軟式野球部',
         id: 'Softball_Baseball',
         isMovie: false,
         movieUrl: '',
         links: [
-            {
-              label: '軟式野球部X（旧Twitter）',
-              url: 'https://twitter.com/suwarikaBBC'
-            },
-            {
-              label: '軟式野球部Instagram',
-              url: 'https://www.instagram.com/sus_baseball/'
-            },
-            {
-              label: '部長連絡先',
-              url: 't124045@ed.sus.ac.jp'
-            },
+          {
+            label: '軟式野球部X（旧Twitter）',
+            url: 'https://twitter.com/suwarikaBBC'
+          },
+          {
+            label: '軟式野球部Instagram',
+            url: 'https://www.instagram.com/sus_baseball/'
+          },
+          {
+            label: '部長連絡先',
+            url: 't124045@ed.sus.ac.jp'
+          }
         ]
       },
-       { clubName: 'フットサル',
+      {
+        clubName: 'フットサル',
         id: 'Futsal',
         isMovie: false,
         movieUrl: '',
-        links: [
-            {
-            },
-        ]
+        links: []
       },
-       { clubName: 'バドミントン部',
+      {
+        clubName: 'バドミントン部',
         id: 'Badminton',
         isMovie: false,
         movieUrl: '',
-        links: [
-            {
-            },
-        ]
+        links: []
       },
-       { clubName: '弓道部',
+      {
+        clubName: '弓道部',
         id: 'Archery',
         isMovie: false,
         movieUrl: '',
-        links: [
-            {
-            },
-        ]
+        links: []
       },
-      { clubName: 'サバイバルゲーム部',
+      {
+        clubName: 'サバイバルゲーム部',
         id: 'Survival_Game',
         isMovie: false,
         movieUrl: '',
         links: [
-            {
-              label: 'サバイバルゲーム部Twitter',
-              url: 'https://x.com/sus_sarvival?s=21&t=1DRjJxI9fQyVarRTvQ_K6A'
-            },
+          {
+            label: 'サバイバルゲーム部Twitter',
+            url: 'https://x.com/sus_sarvival?s=21&t=1DRjJxI9fQyVarRTvQ_K6A'
+          }
         ]
       },
-      { clubName: 'バスケットボール部',
+      {
+        clubName: 'バスケットボール部',
         id: 'Baseketball',
         isMovie: false,
         movieUrl: '',
-        links: [
-            {
-            },
-        ]
+        links: []
       },
-      { clubName: '陸上競技部',
+      {
+        clubName: '陸上競技部',
         id: 'Track_And_Field',
         isMovie: false,
         movieUrl: '',
-        links: [
-            {
-            },
-        ]
-      },
-       
+        links: []
+      }
     ]
   },
   {
-    category: "文化系",
+    category: '文化系',
     clubs: [
-       { clubName: 'e-Sports部',
-        id: 'esport',//名前は適当
+      {
+        clubName: 'e-Sports部',
+        id: 'esport',
         isMovie: true,
-        movieUrl: 'https://www.youtube.com/embed/-1mw_ajWyKA',//埋め込みurl
-        links: [
-          {
-          },
-        ]
+        movieUrl: 'https://www.youtube.com/embed/-1mw_ajWyKA',
+        links: []
       },
-      { clubName: 'TRPG部',
-       id: 'TRPG',
-       isMovie: false,
-       movieUrl: '',
-       links: [
-         {
-         },
-         ]
-      },
-      { clubName: 'リズムゲームサークル',
-        id: 'Rythm_Game',//名前は適当
+      {
+        clubName: 'TRPG部',
+        id: 'TRPG',
         isMovie: false,
         movieUrl: '',
-        links: [
-          {
-          },
-        ]
+        links: []
       },
-      { clubName: 'SuwaGeeks(技術部)',
-        id: 'SuwaGeeks',//名前は適当
+      {
+        clubName: 'リズムゲームサークル',
+        id: 'Rythm_Game',
         isMovie: false,
         movieUrl: '',
-        links: [
-          {
-          },
-        ]
+        links: []
       },
-       { clubName: 'AIサークル',
-        id: 'AI',//名前は適当
+      {
+        clubName: 'SuwaGeeks(技術部)',
+        id: 'SuwaGeeks',
         isMovie: false,
         movieUrl: '',
-        links: [
-          {
-          },
-        ]
+        links: []
       },
-      { clubName: '天文部',
-        id: 'tenmon',//名前は適当
+      {
+        clubName: 'AIサークル',
+        id: 'AI',
+        isMovie: false,
+        movieUrl: '',
+        links: []
+      },
+      {
+        clubName: '天文部',
+        id: 'tenmon',
         isMovie: false,
         movieUrl: '',
         links: [
           {
             label: '天文部Instagram',
             url: 'https://www.instagram.com/sus_astronomical'
-          },
+          }
         ]
       },
-      { clubName: 'アンサンブルサークル',
-        id: 'Ensembl',//名前は適当
+      {
+        clubName: 'アンサンブルサークル',
+        id: 'Ensembl',
         isMovie: false,
         movieUrl: '',
-        links: [
-          {
-          },
-        ]
+        links: []
       },
-      { clubName: '文芸部',
-        id: 'Literature',//名前は適当
+      {
+        clubName: '文芸部',
+        id: 'Literature',
         isMovie: false,
         movieUrl: '',
-        links: [
-          {
-          },
-        ]
+        links: []
       },
-      { clubName: 'ボードゲーム部',
-        id: 'Board_Games',//名前は適当
+      {
+        clubName: 'ボードゲーム部',
+        id: 'Board_Games',
         isMovie: false,
         movieUrl: '',
-        links: [
-          {
-          },
-        ]
+        links: []
       },
-      { clubName: '鉄道研究部',
-        id: 'Railway',//名前は適当
-        isMovie: false,
-        movieUrl: '',
-        links: [
-          {
-            label: '鉄道研究部X',
-            url: 'https://x.com/suwarika_azusa'
-          },
-          {
-            label: '鉄道研究部',
-            url: 'https://www.instagram.com/suwarika_azusa/'
-          },
-        ]
-      },
-      { clubName: '写真部',
-        id: 'Photography',//名前は適当
-        isMovie: false,
-        movieUrl: '',
-        links: [
-          {
-          },
-        ]
-      },
-        ]
-      },
-      { clubName: '軽音学部',
-        id: 'K-On',//名前は適当
-        isMovie: false,
-        movieUrl: '',
-        links: [
-          {
-            label: '軽音学部X',
-            url: 'https://x.com/sus_schoolband?s=21'
-          },
-          {
-            label: '軽音学部Instagraｍ',
-            url: 'https://www.instagram.com/sus_schoolband?igsh=bWtvYWRheHpia2xo'
-          },
-        ]
-      },
-     {
-        clubName: 'チノリカ',
-        id: 'Chino_Rika',
-        isMovie: false,
-        movieUrl: '',
-        links: [
-          {
-            label: 'チノリカいんすた',
-            url: 'https://www.instagram.com/chinorika22?igsh=cmQ2c2xsNmR1aXE2&utm_source=qr'
-          },
-        ]
-      },
-    ]
-  },
-  {
-    category: "外部団体",
-    clubs: []
-  },
-];
+      {
